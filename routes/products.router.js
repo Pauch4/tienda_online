4ffb6b2188f6f4685ec6813dec2db7e1a4432262
CARLOS PEREZ <karloseps08@gmail.com>
2023-08-27 16:11:19 -0600

    se realizo GET, UPDATE, PATCH, DELETE

--- routes/products.router.js
+++ routes/products.router.js
@@ -36,7 +36,24 @@ router.get('/', (req, res) => {
       message: 'created',
       data: body
     });
-  })
-  
+  });
+
+  router.patch('/:id',(req, res)=>{
+    const{id}=req.params;
+    const body = req.body;
+    res.json({
+      message: 'update',
+      data: body,
+      id,
+    });
+  });
+
+  router.delete('/:id',(req, res)=>{
+    const{id}=req.params;
+    res.json({
+      message: 'deleted',
+      id,
+    });
+  });
   module.exports=router;
   
