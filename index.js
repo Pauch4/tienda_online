3f694e6b99d74a135d400a21db5851362c28b7bc
CARLOS PEREZ <karloseps08@gmail.com>
2023-08-27 15:42:22 -0600

    instalamos insomnia y hacemos get y post en el endpoint con un router

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
-const { faker } = require('@faker-js/faker');
+const routerApi = require('./routes');
+
 const app = express();
 const port = 3000;
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hola mi server en express ch4');
@@ -11,54 +14,8 @@ app.get('/nueva-ruta', (req, res) => {
   res.send('Hola, soy una nueva ruta');
 });
 
-app.get('/products', (req, res) => {
-  const products = [];
-  const { size } = req.query;
-  const limit= size || 10;
-  for (let index=0; index < limit; index++){
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(),10),
-      Image:faker.image.imageUrl(),
-    });
-  }
-  res.json(products);
-});
-
-app.get('/products/filter',(req, res)=>{
-  res.send('yo soy un filtro');
-});
-
-app.get('/products/:id', (req, res)=> {
-  const { id } = req.params;
-  res.json({
-    id,
-    name: 'Product 2',
-    price: 2000
-  });
-});
-
-app.get('/users',(req, res)=> {
-  const { limit, offset } = req.query;
-  if(limit && offset) {
-    res.json({
-      limit,
-      offset
-    });
-  } else {
-    res.send('no hay parametros');
-  }
-});
-
-app.get('/categories/:categoryId/products/:productId', (req, res) =>{
-  const { categoryId, productId } = req.params;
-  res.json({
-    categoryId, 
-    productId,
-  });
-})
-
-
 app.listen(port, () => {
   console.log('Mi port' +  port);
 });
+
+routerApi(app);
--- routes/products.router.js
+++ routes/products.router.js
@@ -30,5 +30,13 @@ router.get('/', (req, res) => {
     });
   });
 
+  router.post('/',(req, res)=>{
+    const body = req.body;
+    res.json({
+      message: 'created',
+      data: body
+    });
+  })
+  
   module.exports=router;
   
