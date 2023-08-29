c182a0ebad76e8e9e3a5413793a1fded422a5f4f
Carlos Perez <karloseps08@gmail.com>
2023-08-29 08:59:14 -0600

    instalacion en G15

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,8 @@ const usersRouter=require('./users.router');
 const categoriesRouter=require('./categories.router');
 
 
+
+
 function routerApi(app){
     const router=express.Router();
     app.use('/api/v1', router);
