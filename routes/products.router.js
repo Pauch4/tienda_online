3957f5d7e212287ae47715cded5c05dabd3f83c6
CARLOS PEREZ <karloseps08@gmail.com>
2023-09-03 00:43:00 -0600

    control de integridad de la info con validacio

--- routes/products.router.js
+++ routes/products.router.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const ProductsService = require ('./../services/product.service');
+const validatorHandler=require('./../middlewares/validator.handler')
+const {createProductSchema, updateProductSchema, getProductSchema} = require('./../schemas/product.schema')
 
 const router = express.Router();
 const service = new ProductsService();
@@ -13,8 +15,9 @@ router.get('/', async (req, res) => {
     res.send('yo soy un filtro');
   });
 
-
-  router.get('/:id', async (req, res,next)=> {
+  router.get('/:id', 
+  validatorHandler(getProductSchema, 'params'),
+  async (req, res,next)=> {
     try{
       const { id } = req.params;
       const product = await service.findOne(id);
@@ -22,24 +25,30 @@ router.get('/', async (req, res) => {
     }catch(error){
       next(error);
     }
-   
+
     });
 
-  router.post('/', async (req, res)=>{
-    const body = req.body;
-    const newProduct=await service.create(body);
-    res.status(201).json(newProduct);
-     });
+  router.post('/',
+    validatorHandler(createProductSchema,'body'),
+    async (req, res)=>{
+      const body = req.body;
+      const newProduct=await service.create(body);
+      res.status(201).json(newProduct);
+     }
+  );
 
-  router.patch('/:id', async (req, res, next)=>{
-    try{
-    const{id}=req.params;
-    const body = req.body;
-    const product = await service.update(id, body);
-    res.json(product);
-       }
-    catch(error){
-     next (error);
+  router.patch('/:id',
+  validatorHandler(getProductSchema, 'params'),
+  validatorHandler(updateProductSchema,'body'),
+    async (req, res, next)=>{
+     try{
+     const{id}=req.params;
+     const body = req.body;
+     const product = await service.update(id, body);
+     res.json(product);
+        }
+     catch(error){
+    next (error);
     }
   });
 
